9e5a882a9f41c64226a8c80c87c4d4a68e1c5666
rsensenig <r.a.sensenig@gmail.com>
2022-11-07 21:28:48 -0500

    modified rule variable in recurring-event-controller.js so that the recurring event series contains the first Sunday of every month

--- controllers/recurring-event-controller.js
+++ controllers/recurring-event-controller.js
@@ -2,12 +2,24 @@ const { RRule, RRuleSet, rrulestr } = require('rrule');
 
 // Create a rule:
 const rule = new RRule({
-    freq: RRule.WEEKLY,
-    interval: 5,
-    byweekday: [RRule.MO, RRule.FR],
-    dtstart: new Date(Date.UTC(2012, 1, 1, 10, 30)),
-    until: new Date(Date.UTC(2012, 12, 31))
+    freq: RRule.YEARLY,
+    dtstart: new Date(Date.UTC(2022, 10, 6, 20, 39, 0)),
+    // tzid: America/New_York,
+    until: new Date(Date.UTC(2023, 11, 31, 20, 39, 0)),
+    count: 30,
+    interval: 1,
+    wkst: RRule.SU,
+    byweekday: RRule.SU.nth(+1),
+    bymonth: [1, 2, 3, 4, 5, 9, 10, 11, 12]
   })
+
+// const rule = new RRule({
+//     freq: RRule.WEEKLY,
+//     interval: 5,
+//     byweekday: [RRule.MO, RRule.FR],
+//     dtstart: new Date(Date.UTC(2012, 1, 1, 10, 30)),
+//     until: new Date(Date.UTC(2012, 12, 31))
+//   })
   
   // Get all occurrence dates (Date instances):
   console.log('rule.all(): ', rule.all());
@@ -17,11 +29,9 @@ const rule = new RRule({
   
   // Get an iCalendar RRULE string representation:
   // The output can be used with RRule.fromString().
-  rule.toString()
   console.log('rule.toString(): ', rule.toString());
   
   // Get a human-friendly text representation:
   // The output can be used with RRule.fromText().
-  rule.toText()
   console.log('rule.toText(): ', rule.toText());
 
